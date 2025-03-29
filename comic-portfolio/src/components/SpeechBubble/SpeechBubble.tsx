1a52827f9eefd44f315a98ffba339d521aeb718a
Abdul Rahman Hussain Siddique <Abdulrahman.hussain02@gmail.com>
2025-03-28 23:50:07 -0500

    clean

--- comic-portfolio/src/components/SpeechBubble/SpeechBubble.tsx
+++
@@ -1,9 +0,0 @@
-// Create a SpeechBubble component
-export function SpeechBubble({ text }: { text: string }) {
-    return (
-      <div className="speech-bubble">
-        <div className="bubble-tail" />
-        {text}
-      </div>
-    )
-  }
